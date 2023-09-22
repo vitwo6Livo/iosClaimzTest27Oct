d230b1a35c8d8d4016f0175cae110ed60e1442e5
vitwo6Livo <schatterjee@vitwo.in>
2023-09-22 16:52:06 +0530

    iossss

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/vitwomac/Downloads/flutterTwo"
-export "FLUTTER_APPLICATION_PATH=/Users/vitwomac/Desktop/claimzIosNew/iosClaimz-main"
+export "FLUTTER_APPLICATION_PATH=/Users/vitwomac/Desktop/claimzIosNewThis/iosClaimz-main"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.11"
-export "FLUTTER_BUILD_NUMBER=14"
+export "FLUTTER_BUILD_NAME=1.0.12"
+export "FLUTTER_BUILD_NUMBER=15"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
